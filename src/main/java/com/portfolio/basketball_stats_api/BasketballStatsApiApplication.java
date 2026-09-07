faca7f821b7515bed776b4ba502f7a77bfb6b7b3
Bastibolista <basti.castillodiaz@hotmail.com>
2026-09-07 12:25:30 -0300

    chore: initial project scaffold with Spring Boot 4.1.1 and docker-compose for Postgres

---
+++ src/main/java/com/portfolio/basketball_stats_api/BasketballStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.basketball_stats_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballStatsApiApplication.class, args);
+	}
+
+}
